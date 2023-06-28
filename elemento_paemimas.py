d4fa8174af30359856d2d6e793bb36c766f4fbcf
karina <karina.klinkeviciute@.>
2023-06-28 20:38:03 +0300

    nauji failai

---
+++ elemento_paemimas.py
@@ -0,0 +1,8 @@
+# Access value 20 from the tuple
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+reiksme_20 = tuple1[1][1]
+
+print(reiksme_20)
+
+# papildoma uzduotis: paimti n raide ir paimti skaiciu 25 ir juos atspausdinti
---
+++ tuple_i_kintamuosius.py
@@ -0,0 +1,6 @@
+tuple1 = (1, 2, 3, 4)
+
+# tuple i kintamuosius (unpacking)
+a, b, c, d = tuple1
+
+print(b)
